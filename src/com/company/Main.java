ed5962f3a7ab22d4d2a46efe56bec81d7d0df185
TrungDucMai <trungmaiduc196@gmail.com>
2021-07-07 15:57:26 +0700

    taomoi

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("Gia tri nho nhat la "+ arr[index]);
+    }
+    public static int minValue (int[] array ) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
